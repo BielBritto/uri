c0ded3a07f7f0f79949592db75b7b35ce083fac1
Gabriel <117917203+BielBritto@users.noreply.github.com>
2023-01-02 10:47:31 -0300

    Update 1068 - Balanço de Parênteses I.py

--- 1068 - Balanço de Parênteses I.py
+++ 1068 - Balanço de Parênteses I.py
@@ -3,6 +3,7 @@ while True:
 		entrada = list(input())
 		left = 0
 		right = 0
+		open = 0
 
 		for x in entrada:
 			if x == '(':
@@ -12,7 +13,9 @@ while True:
 				if left > 0:
 					left -= 1
 					right -= 1
+				else:
+					open = 1
 
-		print(['correct' if left == 0 and right == 0 else 'incorrect'][0])
+		print(['correct' if left == 0 and right == 0 and open == 0 else 'incorrect'][0])
 	except:
 		break
